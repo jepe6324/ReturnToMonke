df8bb8e6525b2a5323f7a4ca9538112cac6ca21d
Jesper Persson <jepe6324@student.uu.se>
2021-03-09 13:36:46 +0100

    Playable version

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -6,22 +6,37 @@ class Collisions
 {
 	public enum Direction
 	{
+		NONE = 0,
 		LEFT = 1,
 		RIGHT = 2,
 		UP = 4,
 		DOWN = 8
 	}
-	bool IsCollisionWithDirection(Direction direction, Collision2D collision)
+	public bool IsCollisionInDirection(Direction direction, Collision2D collision)
 	{
 		for (int i = 0; i < collision.GetContacts(collision.contacts); i++)
 		{
 			Vector2 normal = collision.GetContact(i).normal;
-			
+			Direction normalDirection = VectorToDirection(normal);
+			if ((direction & normalDirection) == direction) {
+				return true;
+			}
 		}
 		return false;
 	}
-	Direction VectorToDirection(Vector2 vector)
+	private Direction VectorToDirection(Vector2 vector)
 	{
+		Direction direction = Direction.NONE;
+		if (vector.x > 0.5) {
 
+			direction |= Direction.LEFT;
+		}else if (vector.x < -0.5) {
+			direction |= Direction.RIGHT;
+		}
+		if (vector.y > 0.5) {
+			direction |= Direction.DOWN;
+		}else if (vector.y < -0.5) {
+			direction |= Direction.UP;
+		} return direction;
 	}
 }
