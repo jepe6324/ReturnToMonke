392a728cee3e5697eca9b45c31ce9990673e98fb
Jesper Persson <jepe6324@student.uu.se>
2021-03-15 11:44:45 +0100

    player and companion sprites changed, z-layer change to certain objects

---
+++ Assets/Scripts/CompanionSpriteSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionSpriteSelector : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer_;
+    CompanionMovement movement_;
+
+    public Sprite runSprite, jumpSprite, hangSprite;
+    void Start()
+    {
+        spriteRenderer_ = GetComponent<SpriteRenderer>();
+        movement_ = GetComponent<CompanionMovement>();
+    }
+    void Update()
+    {
+        
+    }
+}
